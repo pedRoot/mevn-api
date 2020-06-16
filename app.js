d9ac3e02621cca4322c686692b06614fa83bd851
pedroot <pedrotorres@outlook.com>
2020-06-16 14:45:11 -0400

    Conecta con MongoDB

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import logger from "morgan";
 import path from "path";
 import cors from "cors";
+import mongoose from "mongoose";
 
 const app = express();
 
@@ -12,11 +13,6 @@ app.use(express.urlencoded({extended: true}));
 
 //app.use("/api", require("./routes"));
 
-// app.get('/', (req, res) => {
-//   console.log("Hello world...");
-//   res.send("Hello World!!");
-// });
-
 // Todo este bloque debe estar junto y debajo de las rutas
 import history from  "connect-history-api-fallback";
 app.use(history());
@@ -26,5 +22,13 @@ app.set("port", process.env.PORT || 8000);
 const port = app.get("port");
 
 app.listen(port, () => {
-  console.log(`My app listing on http://localhost:${port}`);
+  console.log(`==> My app listing on http://localhost:${port}`);
 });
+
+const uri = 'mongodb://localhost:27017/mevn';
+const options = {useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true};
+
+mongoose.connect(uri, options).then(
+  () => {  console.log('==> Conectado a MongoDB')  },
+  err => { console.log(`==> Error al conectar con mongo ${err}`);}
+);
